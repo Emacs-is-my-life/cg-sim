4524e774202536724871396b2fca80f3f6a9e1a4
GUIXXER <emacs.is.my.life@gmail.com>
2026-04-03 03:41:01 +0900

    Before Aider

--- sim/sched/flexinfer/flexinfer.py
+++ sim/sched/flexinfer/flexinfer.py
@@ -5,14 +5,16 @@ from typing import Any, TYPE_CHECKING
 
 from sim.sched.common import BaseScheduler
 from sim.core.log import Log
-from sim.core.trace import Trace
+from sim.core.trace import Trace, Tensor
 from sim.core.job import BaseJob, ComputeJob, ClaimJob, ReleaseJob, TransferJob
 from sim.hw.compute.common import BaseCompute
-from sim.hw.memory.common import BaseMemory
-from sim.hw.storage.common import BaseStorage
+from sim.hw.memory.common import BaseMemory, MemoryRegion
+from sim.hw.storage.common import BaseStorage, StorageRegion
 
 if TYPE_CHECKING:
     from sim.core.system import System
+
+from .utils import categorize_tensors
 
 
 class FlexInferMode(Enum):
@@ -29,6 +31,11 @@ class FlexInfer(BaseScheduler):
 
     def __init__(self, obj_id: int, name: str, log: Log, sys: System, args: dict[str, Any] | None = None):
         super().__init__(obj_id, name, log, sys, args)
+        self.prefetch_window = 3  # 3 Layers by default, in paper
+        if "prefetch_window" in args:
+            self.prefetch_window = int(args["prefetch_window"])
+            if self.prefetch_window < 1:
+                raise Exception(f"[FlexInfer] Prefetch window cannot be: {self.prefetch_window}")
 
         """
         Assumption: Expect one of each hardware:
@@ -45,14 +52,54 @@ class FlexInfer(BaseScheduler):
             elif isinstance(hw, BaseStorage):
                 self.storage = hw
 
-        # Check tensors in sys.trace.tensor_map,
-        # see if memory.space size is sufficient,
-        # set self.mode: FlexInferOpMode = <MODE> accordingly.
+        # Tensor information gather
+        self.layers, self.tensor_layer_num, others_size_num_pages = categorize_tensors(sys.trace.tensor_map)
+        attn_tensor_size = self.layers[0].attn[0].num_pages
+        ffn_tensor_size = self.layers[0].ffn[0].num_pages
+        num_layers = len(self.layers)
+
+        # Check operation mode
+        memory_size = self.memory.space.num_total_pages
+        minimum_size = self.prefetch_window * (3 * ffn_tensor_size + 2 * attn_tensor_size) + others_size_num_pages
+        if memory_size < minimum_size:
+            args = {
+                "from": self.name,
+                "msg": f"Memory size is too small to run FlexInfer policy. {4 * minimum_size} KB required, have {4 * memory_size} KB."
+            }
+            sys.abort(args)
+
+        args = {"from": self.name, "msg": ""}
+        if (memory_size > (3 * num_layers * ffn_tensor_size + 2 * num_layers * attn_tensor_size) + others_size_num_pages):
+            self.mode = FlexInferMode.MEMORY_SUFFICIENT
+            args["msg"] = f"FlexInfer Scheduler operating in MEMORY_SUFFICIENT mode."
+        elif (memory_size > num_layers * ffn_tensor_size + others_size_num_pages):
+            self.mode = FlexInferMode.MEMORY_INTERMEDIATE
+            args["msg"] = f"FlexInfer Scheduler operating in MEMORY_INTERMEDIATE mode."
+        else:
+            self.mode = FlexInferMode.MEMROY_LIMITED
+            args["msg"] = f"FlexInfer Scheduler operating in MEMORY_LIMITED mode."
+
+        self.log(Log.engine(self.id, "SCHEDULER_MESSAGE", 0, args))
+
+
+        self.attn_tensor_size = attn_tensor_size
+        self.ffn_tensor_size = ffn_tensor_size
+
+        self.must_reserve_pages = self.prefetch_window * (3 * ffn_tensor_size + 2 * attn_tensor_size)
+
+        self.page_idx_heap_start = 0
+        self.page_idx_heap_end = self.memory.space.total_num_pages
         return
 
     def compile(self, trace: Trace) -> None:
         """No compilation"""
         return
+
+    def _build_payload(self, init_storage: BaseStorage, tensor: Tensor) -> tuple[MemoryRegion, StorageRegion]:
+        mem_region = self.sys.claim(self.memory, tensor, self.page_idx_heap_start)
+        self.page_idx_heap_start += tensor.num_pages
+        stor_region = self.sys.find(init_storage, tensor)[0]
+        return (mem_region, stor_region)
 
     def layout(self, init_storage: BaseStorage) -> None:
         """
@@ -74,21 +121,50 @@ class FlexInfer(BaseScheduler):
         13: return 𝑃
         """
 
-        """
-        - Considering self.mode,
-        - see tensor name and type,
-        - load tensors from init_storage to memory
-        - according to FlexInfer layout algorithm above
-        """
+        tensor_map = self.sys.trace.tensor_map
+        others_batch = []
+        # Load/Claim All 'other' tensors in memory
+        for tensor_id, layer_num in self.tensor_layer_num:
+            if layer_num == -1:
+                tensor = tensor_map[tensor_id]
+                if tensor.args["tensor_type"] == "INTERMEDIATE":
+                    # Claiming memory region is enough
+                    self.sys.claim(self.memory, tensor, self.page_idx_heap_start)
+                    self.page_idx_heap_start += tensor.num_pages
+                else:
+                    # Claim a memory region, and load tensor from the storage
+                    mem_region = self.sys.claim(self.memory, tensor, self.page_idx_heap_start)
+                    self.page_idx_heap_start += tensor.num_pages
+                    stor_region = self.sys.find(init_storage, tensor)[0]
+                    others_batch.append((mem_region, stor_region))
 
+        # Pin Attn/FFN tensors
+        pin_batch = []
+        if self.mode == FlexInferMode.MEMORY_SUFFICIENT:
+            for layer in self.layers:     # For every layer,
+                for tensor in layer.ffn:  # Pin all FFN Tensors and one Attn Tensor
+                    pin_batch.append(self._build_payload(init_storage, tensor))
+
+                pin_batch.append(self._build_payload(init_storage, layer.attn[0]))
+
+        if self.mode == FlexInferMode.MEMORY_INTERMEDIATE:
+            num_pages_avail = (self.page_idx_heap_end - self.page_idx_heap_start) - self.must_reserve_pages
+
+            # TODO: Implement
+            # Consider self.attn_tensor_size, self.ffn_tensor_size and num_pages_avail,
+            # 1. Try to pin FFN tensors in memory, in uniform manner (all layers have similar number of pinned FFN tensors)
+            # 2. If space is still available, pin Attn tensors as much as possible, in uniform manner
+            pass
+        if self.mode == FlexInferMode.MEMROY_LIMITED:
+            num_pages_avail = (self.page_idx_heap_end - self.page_idx_heap_start) - self.must_reserve_pages
+
+            # TODO: Implement
+            # Consider self.attn_tensor_size, self.ffn_tensor_size and num_pages_avail,
+            # 1. Try to pin Attn tensors in memory, in uniform manner
+            pass
+
+        self.sys.transfer(others_batch + pin_batch)
         return
 
     def runtime(self, retired_jobs: list[BaseJob]) -> None:
-        """
-        - Don't touch MemoryRegions for pinned tensors
-        - Release MemoryRegions holding spent tensors (and will not be used soon)
-        - Claim Memory Regions which will be used in future, as much as memory permits
-        - Submit compute jobs, which can be run
-        """
-
         return
--- sim/sched/flexinfer/utils.py
+++ sim/sched/flexinfer/utils.py
@@ -1,46 +1,51 @@
 from sim.core.trace import Tensor
+import re
 
 
 class Layer:
     def __init__(self, layer_number: int):
         self.N = layer_number
-        self.attn: list[Tensor]
-        self.ffn: list[Tensor]
+        self.attn: list[Tensor] = []
+        self.ffn: list[Tensor] = []
         return
 
 
-def categorize_tensors(tensor_map: dict[int, Tensor]) -> (list[Layer], dict[int, int], int):
+def categorize_tensors(tensor_map: dict[int, Tensor]) -> tuple[list[Layer], dict[int, int], int]:
     others_size_num_pages: int = 0
     tensor_layer: dict[int, int] = {}
 
-    max_layer = 0
-    for tensor in tensor_map.values():
-        # if this tensor is part of a layer:
-        #    tensor.name is like: blk.<layer>.*
-        #    Get layer_num
-        #    if layer_num > max_layer:
-        #      max_layer = layer_num
-        pass
+    layer_pattern = re.compile(r"^blk\.(\d+)\.")
 
-    layers = [Layer(i) for i in range(max_layer)]
+    max_layer = -1
+    for tensor in tensor_map.values():
+        match = layer_pattern.match(tensor.name)
+        if match is not None:
+            layer_num = int(match.group(1))
+            if layer_num > max_layer:
+                max_layer = layer_num
+
+    layers = [Layer(i) for i in range(max_layer + 1)]
+
     for tensor in tensor_map.values():
         name = tensor.name
-        # if [tensor.args["tensor_type"] == "WEIGHT"  and from name, check if this tensor is part of a layer]
-        #    n_this_layer = [Pull layer number from the tensor name]
-        #    layer = layers[n_this_layer]
-        #
-        #    if ["attn_q" or "attn_output" in name]
-        #      layer.attn.append(tensor)
-        #      tensor_layer[tensor.id] = n_this_layer
-        #    elif ["ffn_down" or "ffn_gate" or "ffn_up" in name]
-        #      layer.ffn.append(tensor)
-        #      tensor_layer[tensor.id] = n_this_layer
-        #    else
-        #      others_size_num_pages += tensor.num_pages
-        #      tensor_layer[tensor.id] = -1
-        # else
-        #      others_size_num_pages += tensor.num_pages
-        #      tensor_layer[tensor.id] = -1
-        pass
+        tensor_type = tensor.args.get("tensor_type")
 
-    return
+        match = layer_pattern.match(name)
+        if tensor_type == "WEIGHT" and match is not None:
+            n_this_layer = int(match.group(1))
+            layer = layers[n_this_layer]
+
+            if "attn_q" in name or "attn_output" in name:
+                layer.attn.append(tensor)
+                tensor_layer[tensor.id] = n_this_layer
+            elif "ffn_down" in name or "ffn_gate" in name or "ffn_up" in name:
+                layer.ffn.append(tensor)
+                tensor_layer[tensor.id] = n_this_layer
+            else:
+                others_size_num_pages += tensor.num_pages
+                tensor_layer[tensor.id] = -1
+        else:
+            others_size_num_pages += tensor.num_pages
+            tensor_layer[tensor.id] = -1
+
+    return layers, tensor_layer, others_size_num_pages
