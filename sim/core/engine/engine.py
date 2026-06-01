83feb0a69491250dc64c2013df11d75d62733bd8
GUIXXER <emacs.is.my.life@gmail.com>
2026-06-01 19:50:59 +0900

    Fixed fixed_latency in transfer job being ignored. Also removed retiring multiple jobs with same ETA. Only one job at a time retires.

--- sim/core/engine/engine.py
+++ sim/core/engine/engine.py
@@ -248,18 +248,13 @@ class Engine(SimObject):
             if isinstance(job, TransferJob) and (job.fixed_latency_micros > 0.0):
                 if not job.bw_work_complete:
                     job.bw_work_complete = True
+                    job.timestamp_ETA = self.timestamp_now + job.fixed_latency_micros
                     heapq.heappush(self.job_running, job)
                 else:
                     # Fixed latency is handled, so retire this TransferJob
                     job.end(self.log, self.sys, self.timestamp_now)
                     retired_jobs.append(job)
             else:
-                job.end(self.log, self.sys, self.timestamp_now)
-                retired_jobs.append(job)
-
-            # Retire jobs with the same ETA timestamp
-            while self.job_running and (self.job_running[0].timestamp_ETA == self.timestamp_now):
-                job = heapq.heappop(self.job_running)
                 job.end(self.log, self.sys, self.timestamp_now)
                 retired_jobs.append(job)
 
